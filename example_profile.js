db906132fb6b8c768ab20edd0c1db9aacfe47eb5
Aneury Casado <aneury.casado@gmail.com>
2015-07-01 06:55:20 -0400

    Got the user route to display current information about users.

--- example_profile.js
+++ example_profile.js
@@ -1,14 +1,14 @@
 var Profile = require("./profile.js");
 
 
-var studentProfile = new Profile("chalkers");
+
 
 /**
-* When the JSON body is fully recieved the 
+* When the JSON body is fully recieved the
 * the "end" event is triggered and the full body
 * is given to the handler or callback
 **/
-studentProfile.on("end", console.dir);
+
 
 /**
 * If a parsing, network or HTTP error occurs an
--- router.js
+++ router.js
@@ -1,3 +1,6 @@
+var Profile = require("./profile.js");
+
+
 function home(request,response){
 
     if(request.url === "/"){
@@ -13,8 +16,23 @@ function user(request,response){
   if(username.length > 0){
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.write("Header\n");
+    var studentProfile = new Profile(username);
+    studentProfile.on("end", function(profileJSON){
+      var values = {
+        avatarUrl: profileJSON.gravater_url,
+        username: profileJSON.profile_name,
+        badges: profileJSON.badges.length,
+        JavaScriptPoints: profileJSON.points.JavaScript
+      }
+      response.write(values.username + "has " + values.badges + " badges\n")
+      response.end("Footer\n");
+
+    });
+    studentProfile.on("error",function(error){
+      response.write(error.message + "\n")
+      response.end("Footer\n");
+    });
     response.write(username + "\n");
-    response.end("Footer\n");
   }
 }
 
